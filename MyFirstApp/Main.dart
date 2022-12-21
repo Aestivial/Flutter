0fa9bc46ff3a3fd7c2df96ec30f90eed71af73d1
Nayan Panda <69592060+Aestivial@users.noreply.github.com>
2022-12-22 00:44:49 +0530

    Stateless update for supporting hot reload.

--- MyFirstApp/Main.dart
+++ MyFirstApp/Main.dart
@@ -2,7 +2,14 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Scaffold(
+    home: Home(),
+  ));
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
         title: Text('Here live dragons'),
@@ -11,21 +18,21 @@ void main() {
       ),
       body: Center(
         child: Text(
-            'Hello Aest!',
-            style: TextStyle(
-              fontSize: 45.0,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 2.0,
-              color: Colors.grey[700],
-              fontFamily: 'Ubuntu',
-            ),
+          'Hello Aest!',
+          style: TextStyle(
+            fontSize: 45.0,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2.0,
+            color: Colors.grey[700],
+            fontFamily: 'Ubuntu',
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: (){},
         child: Text('Click!'),
         backgroundColor: Colors.black,
-        ),
       ),
-  ));
+    );
+  }
 }
