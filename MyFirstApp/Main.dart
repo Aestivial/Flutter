2003f6e469df861164e02769839a0790241af882
Nayan Panda <69592060+Aestivial@users.noreply.github.com>
2022-12-22 00:35:19 +0530

    Added colors, custom font and other updates.

--- MyFirstApp/Main.dart
+++ MyFirstApp/Main.dart
@@ -3,16 +3,28 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     home: Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
         title: Text('Here live dragons'),
         centerTitle: true,
+        backgroundColor: Colors.black,
       ),
       body: Center(
-        child: Text('Hello Aest!'),
+        child: Text(
+            'Hello Aest!',
+            style: TextStyle(
+              fontSize: 45.0,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 2.0,
+              color: Colors.grey[700],
+              fontFamily: 'Ubuntu',
+            ),
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: (){},
         child: Text('Click!'),
+        backgroundColor: Colors.black,
         ),
       ),
   ));
