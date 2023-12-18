609daa9866843f8baf76d95a787731390695809a
Nayan Panda <69592060+Aestivial@users.noreply.github.com>
2023-12-18 23:05:50 +0530

    Create functions.dart

---
+++ MyAuth/App/Custom Functions/functions.dart
@@ -0,0 +1,22 @@
+import 'dart:convert';
+import 'dart:math' as math;
+
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:intl/intl.dart';
+import 'package:timeago/timeago.dart' as timeago;
+import 'lat_lng.dart';
+import 'place.dart';
+import 'uploaded_file.dart';
+import '/backend/backend.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import '/auth/firebase_auth/auth_util.dart';
+
+double? inchesToCms(double? inches) {
+  // write a function to convert inches to cms
+  if (inches == null) {
+    return null;
+  } else {
+    return inches * 2.54;
+  }
+}
